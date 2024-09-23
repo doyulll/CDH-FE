6dbc8f7194db4f0e73582fb8bb8e414c2eeb6388
doyulll <ajin7385@naver.com>
2024-09-23 20:59:10 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Sidebar from './components/sidebar';
+import MainPage from './pages/Main/MainPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // 추가
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+          {<Sidebar/>}
+        <Routes>
+            {/* 메인 페이지 */}
+          <Route path="/" element={<MainPage />} />
+
+
+
+
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
